4c2323c9b25104accee77c66f5008e78fb76280e
Seamus Mullan <43112447+SeamusMullan@users.noreply.github.com>
2025-12-11 22:29:16 +0000

    module thing

---
+++ Lab5/src/Module.h
@@ -0,0 +1,33 @@
+#ifndef MODULE_H
+#define MODULE_H
+
+#include <string>
+
+class Module {
+public:
+    // empty constrcutr
+    Module();
+
+    // default 2 creds and 0 students
+    Module(std::string name, unsigned int credits = 2, unsigned int students = 0);
+
+    std::string getName() const;
+    void setName(std::string name);
+
+    unsigned int getStudents() const;
+    void setStudents(unsigned int students);
+
+    unsigned int getCredits() const;
+    void setCredits(unsigned int credits);
+
+    bool operator==(const Module& someModule) const;
+
+
+private:
+    std::string _name;
+    unsigned int _students;
+    unsigned int _credits;
+
+};
+
+#endif
